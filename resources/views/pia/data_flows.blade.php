5aedbe3dd38627e3364a1d0b8c52fb7d9f0199a4
edjerygumbao <egcgumbao@usep.edu.ph>
2023-06-21 16:17:08 +0800

    Data flow and PIA update + CSS

--- resources/views/pia/data_flows.blade.php
+++ resources/views/pia/data_flows.blade.php
@@ -7,8 +7,6 @@
 @if($value == 1)
     <p>Welcome to the Data Flows</p>
 
-    <p>!! Under Construction !!</p>
-
     <img src="img/sample_data_flow.png" alt="Sample Data Flow" width="600" height="300">
 
     <p><em>Figure 1. Information flow of personal information can be visualized in a work flow diagram on personal information processing.</em></p>
@@ -51,9 +49,96 @@
     </ul>
 
     <p><em>Identify the personal data involved and describe the data flow from collection to disposal by answering the following questions below:</em></p>
+    <ul>
+        <p><strong>What personal data are being or will be processed by this project/system?</strong></p>
+        <ul>
+            <p>List all personal data (e.g. Personal Full Name, address, gender, phone number, etc.,) and state which is/are the sensitive personal information (e.g. race, ethnicity, marital status, health, genetic, government issued numbers).</p>
+        </ul>
 
-    <li></li>
+        <p><strong>All the information stated above will be in accordance to the next section.</strong></p>
 
+        <ul>
+            <h5><strong>Collection</strong></h5>
+            <ol>
+                <li>State who collected or will be collecting the personal information and/or sensitive information.</li>
+                <li>How the personal information/sensitive personal information is collected and from whom it was collected?</li>
+                <ul>
+                    <li><em>If personal information is collected from some source other than the individual?</em></li>
+                </ul>
+                <li>What is/are the purpose(s) of collecting the personal data?</li>
+                <ul>
+                    <li><em>Be clear about the purpose of collecting the information</em></li>
+                    <li><em>Are you collecting what you only need?</em></li>
+                </ul>
+                <li>How was or will the consent be obtained?</li>
+                <ul>
+                    <li><em>Do individuals have the opportunity and/or right to decline to provide data?</em></li>
+                    <li><em>What happen if they decline?</em></li>
+                </ul>
+            </ol>
+            
+            <h5><strong>Storage</strong></h5>
+            <ol>
+                <li>Where is it currently being stored?</li>
+                <ul>
+                    <li><em>Is it being stored in a physical server or in the cloud?</em></li>
+                </ul>
+                <li>Is it being stored in other country</li>
+                <ul>
+                    <li><em>If it is subject to a cross-border transfer, specify what country or countries.</em></li>
+                </ul>
+                <li>Is the storage of data being outsourced?</li>
+                <ul>
+                    <li><em>Specify if the storing process is being done in-house or is it handled by a service provider</em></li>
+                </ul>
+            </ol>
+
+            <h5><strong>Usage</strong></h5>
+            <ol>
+                <li>How will the data being used or what is the purpose of its processing?</li>
+                <ul>
+                    <li><em>Describe how the collected information is being used or will be used</em></li>
+                    <li><em>Specify the processing activities where the personal information is being used.</em></li>
+                </ul>
+            </ol>
+
+            
+            <h5><strong>Retention</strong></h5>
+            <ol>
+                <li>How long are the data being retained? And Why?</li>
+                <ul>
+                    <li><em>State the length of period the data is being retained?</em></li>
+                    <li><em>What is the basis of retaining the data that long? Specify the reason(s)</em></li>
+                </ul>
+                <li>How long are the data being retained? And Why?</li>
+                <ul>
+                    <li><em>Specify if the data retention process is being done in-house or is it handled by a service provider</em></li>
+                </ul>
+            </ol>
+
+            <h5><strong>Disclosure/Sharing</strong></h5>
+            <ol>
+                <li>To whom it is being disclosed to?</li>
+                <li>Is it being disclosed outside the organization? Why is it being disclosed?</li>
+                <ul>
+                    <li><em>Specify if the personal information is being shared outside the organization</em></li>
+                    <li><em>What are the reasons for disclosing the personal information</em></li>
+                </ul>
+            </ol>
+
+            <h5><strong>Disposal/Destruction</strong></h5>
+            <ol>
+                <li>How will the data be disposed?</li>
+                <ul>
+                    <li><em>Describe the process of disposing the personal information</em></li>
+                </ul>
+                <li>Who will facilitate the destruction of the data?</li>
+                <ul>
+                    <li><em>State if the process is being managed in-house or if it is a third party</em></li>
+                </ul>
+            </ol>
+        </ul>
+    </ul>
 
     <form action="proceed_to_privacy_impact_analysis" method="post">
         @csrf
